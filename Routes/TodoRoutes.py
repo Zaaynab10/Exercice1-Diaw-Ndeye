f6c487b2123c77cd3bfb4455cc580ad2c4df71da
Zaaynab10 <116828252+Zaaynab10@users.noreply.github.com>
2025-10-29 14:38:15 +0100

    `Refactor TotoRoutes to use Flask instead of http.server`

--- Routes/TodoRoutes.py
+++ Routes/TodoRoutes.py
@@ -1,29 +1,30 @@
+from flask import Blueprint, request, jsonify
 from Controller.TodoController import TodoController
+
 from Model.TodoModel import Todo
 
-class Router:
-    def __init__(self):
-        self.controller = TodoController(Todo())
+todo_controller = TodoController(Todo())
 
-    def handle(self, method, path, data=None):
-        if method == "GET" and path == "/todos":
-            return self.controller.get_all()
+todo_bp = Blueprint("todo_bp", __name__)
 
-        elif method == "POST" and path == "/todos":
-            return self.controller.create(data)
+@todo_bp.route("/todos", methods=["GET"])
+def get_todos():
+    res = todo_controller.get_all()
+    return jsonify(res), res["status"]
 
-        elif method == "PATCH" and path.startswith("/todos/"):
-            try:
-                id = int(path.split("/")[2])
-                return self.controller.update(id, data)
-            except:
-                return {"status": 400, "error": "ID invalide"}
+@todo_bp.route("/todos", methods=["POST"])
+def create_todo():
+    data = request.json
+    res = todo_controller.create(data)
+    return jsonify(res), res["status"]
 
-        elif method == "DELETE" and path.startswith("/todos/"):
-            try:
-                id = int(path.split("/")[2])
-                return self.controller.delete(id)
-            except:
-                return {"status": 400, "error": "ID invalide"}
+@todo_bp.route("/todos/<int:id>", methods=["PATCH"])
+def update_todo(id):
+    data = request.json
+    res = todo_controller.update(id, data)
+    return jsonify(res), res["status"]
 
-        return {"status": 404, "error": "Route non trouvée"}
+@todo_bp.route("/todos/<int:id>", methods=["DELETE"])
+def delete_todo(id):
+    res = todo_controller.delete(id)
+    return jsonify(res), res["status"]
